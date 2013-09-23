15c297a394af9f31e970ed29687e6ebcea29a477
iamtheoneorg <jul@MacBook-Air-de-Catherine.local>
2013-09-23 13:49:12 -0400

    units class logic fix

--- src/com/iamtheoneorg/convertthis/units/Feet.java
+++ src/com/iamtheoneorg/convertthis/units/Feet.java
@@ -10,6 +10,9 @@ public class Feet extends Units {
         super.getConvertions().put("cm",30.48);
         super.getConvertions().put("m",0.3048);
         super.getConvertions().put("km",0.0003048);
+        super.getConvertions().put("arpent",0.00520833);
+		super.getConvertions().put("inch",12.0);
+		super.getConvertions().put("yard",0.333333333);
 	}
 	
 
--- src/com/iamtheoneorg/convertthis/units/Inch.java
+++ src/com/iamtheoneorg/convertthis/units/Inch.java
@@ -11,6 +11,9 @@ public class Inch extends Units{
 		super.getConvertions().put("cm",2.54);
 		super.getConvertions().put("m",0.0254);
 		super.getConvertions().put("km",2.54E-5);
+		super.getConvertions().put("arpent",0.000434028);
+		super.getConvertions().put("feet",0.0833333333);
+		super.getConvertions().put("yard",0.0277777778);
 		
 		
 		
